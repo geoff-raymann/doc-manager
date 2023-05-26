1d0dc59a565b5a9bb4491f0653cdfbbd070a60d0
geoff-raymann <odiwuorgeoff50@gmail.com>
2023-05-26 11:17:54 +0300

    Add React.js

---
+++ document-management-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-3xl font-bold mb-4">Document Management Application</h1>
+      <p>Welcome to the document management app!</p>
+    </div>
+  );
+}
+
+export default App;
+
+
+
---
+++ document-management-app/src/components/DocumentList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function DocumentList() {
+  return (
+    <div>
+      <h2>Document List</h2>
+      <ul>
+        <li>Document 1</li>
+        <li>Document 2</li>
+        <li>Document 3</li>
+      </ul>
+    </div>
+  );
+}
+
+export default DocumentList;
